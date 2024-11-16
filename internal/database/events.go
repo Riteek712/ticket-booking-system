c072a31127026a4e01f71da5e2760374778726d5
Riteek Rakesh <riteek7.12@gmail.com>
2024-11-16 23:03:31 +0530

    added user model

--- internal/database/events.go
+++ internal/database/events.go
@@ -4,17 +4,19 @@ import (
 	"gorm.io/gorm"
 )
 
-// Event represents the event model in the database.
+// Event represents the event model in the database.// Event represents the event model in the database.
 type Event struct {
 	gorm.Model  `swaggerignore:"true"`
-	Name        string `gorm:"type:varchar(255);not null"`
-	Description string `gorm:"type:text;not null"`
-	EventID     string `gorm:"type:varchar(255);unique;not null"`
-	Capacity    int    `gorm:"not null"`
+	Name        string   `gorm:"type:varchar(255);not null"`        // Name of the event
+	Description string   `gorm:"type:text;not null"`                // Event description
+	EventID     string   `gorm:"type:varchar(255);unique;not null"` // Unique event identifier
+	Capacity    int      `gorm:"not null" json:"capacity"`          // Total capacity of the event
+	Tickets     []Ticket `gorm:"foreignKey:EventID" json:"tickets"` // Associated tickets
 }
 
+// CreateEventDTO represents the data for creating a new event.
 type CreateEventDTO struct {
-	Name        string `json:"name" validate:"required"`
-	Description string `json:"description" validate:"required"`
-	Capacity    int    `json:"capacity" validate:"required"`
+	Name        string `json:"name" validate:"required,min=3,max=255"` // Event name
+	Description string `json:"description" validate:"required"`        // Event description
+	Capacity    int    `json:"capacity" validate:"required,min=1"`     // Total capacity of the event
 }
