038d3dbe715283f3fddc576fe66c959d1f9ce8e3
Riteek Rakesh <riteek7.12@gmail.com>
2025-02-22 20:30:00 +0530

    setup db in docker

--- internal/database/database.go
+++ internal/database/database.go
@@ -48,8 +48,9 @@ func New() (Service, error) {
 		return dbInstance, nil
 	}
 
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable search_path=%s",
-		host, username, password, database, port, schema)
+	// dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable search_path=%s",
+	// 	host, username, password, database, port, schema)
+	dsn := "host=localhost user=admin password=yourpassword dbname=ticketing port=5432 sslmode=disable"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect to database: %w", err)
